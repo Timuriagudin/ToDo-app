c9d3cbc14839a8909ad43cb406cfbb6cdfee0172
Tim <timuriagudin@yahoo.com>
2024-08-14 18:52:10 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import TodosContainer from "./containers/TodosContainer"
-import ContactContainer from "./containers/ContactContainer"
-import { TodoProvider } from './context/TodoContext';
+import TodosContainer from "./containers/TodosContainer";
+import ContactContainer from "./containers/ContactContainer";
+import Footer from "./components/Footer";
+import { TodoProvider } from "./context/TodoContext";
+import "./App.css";
 
-
-function App() {
+const App = () => {
   return (
     <Router>
       <TodoProvider>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/todos" element={<TodosContainer />} />
-          <Route path="/contact" element={<ContactContainer />} />
-          <Route path="/" element={<h1>Welcome</h1>} />
-        </Routes>
-      </div>
+        <div id="root">
+          <NavBar />
+          <main>
+            <Routes>
+              <Route path="/todos" element={<TodosContainer />} />
+              <Route path="/contact" element={<ContactContainer />} />
+              <Route path="/" element={<h1>Welcome</h1>} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </TodoProvider>
     </Router>
   );
-}
+};
 
 export default App;
