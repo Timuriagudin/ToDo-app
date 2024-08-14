1576544127ce5170040b944a8d9188f38e44a05e
Tim <timuriagudin@yahoo.com>
2024-08-14 17:06:19 -0400

    added todo form

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./TodoForm.css";
+
+export default function TodoForm({ onAdd }) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim()) {
+      onAdd(text);
+      setText("");
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
