005934856a63f997f5de294b2e8df665a8e55d63
Tim <timuriagudin@yahoo.com>
2024-09-01 18:15:43 -0400

    added login form

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,11 +3,26 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the ultimate task management app!</h1>
-      <h2>
-        Here, you can create, edit, update the status, or remove your tasks.
-      </h2>
+    <div className="container">
+      <div className="left-section">
+        <h1>Welcome to the ultimate task management app!</h1>
+        <h2>
+          Here, you can create, edit, update the status, or remove your tasks.
+        </h2>
+      </div>
+      <div className="right-section">
+        <form className="login-form">
+          <input type="email" name="email" placeholder="email" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            required
+          />
+          <button type="submit">Sign in</button>
+          <button type="button">Sign up</button>
+        </form>
+      </div>
     </div>
   );
 };
